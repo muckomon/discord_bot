0d250927750860868167ed6946a663ffffbbdbbf
muckomon <112518552+muckomon@users.noreply.github.com>
2023-04-26 16:25:45 +0200

    Update and rename heroes.py to questions.py

---
+++ questions.py
@@ -0,0 +1,192 @@
+trivia_data = [
+    {
+        "question": "Which hero says “Justice rains from above!” when using their ultimate ability?",
+        "answers": ["Pharah", "Mercy", "Reinhardt", "Soldier 76"],
+        "correct": 0  # The index of the correct answer in the list
+    },
+    {
+        "question": "Which hero is a former professional gamer from South Korea?",
+        "answers": ["D.Va", "Mei", "Genji", "Sombra"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is the leader of the Deadlock Gang?",
+        "answers": ["Cassidy", "Ashe", "Roadhog", "Reaper"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is a former member of the Australian Liberation Front?",
+        "answers": ["Symmetra", "Zenyatta", "Junkrat", "Bastion"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a former Talon operative who defected to Overwatch?",
+        "answers": ["Widowmaker", "Moira", "Genji", "Baptiste"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is a former Overwatch agent who was presumed dead after a mission in Venice?",
+        "answers": ["Ana", "Reinhardt", "Soldier 76", "Winston"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is a hacker who was part of the Los Muertos gang in Mexico?",
+        "answers": ["Sombra", "Echo", "Ashe", "Hanzo"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is a former Overwatch agent who was the leader of Blackwatch?",
+        "answers": ["Moira", "Genji", "Reaper", "Tracer"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero was the test pilot of the Slipstream project?",
+        "answers": ["Pharah", "Mercy", "Winston", "Tracer"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero was the mentor of Genji?",
+        "answers": ["Hanzo", "Zenyatta", "Reinhardt", "Lifeweaver"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is the daughter of Torbjörn?",
+        "answers": ["Bastion", "Mei", "Brigitte", "Echo"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero has the highest base health pool?",
+        "answers": ["Reinhardt", "Roadhog", "Wrecking Ball", "D.Va"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero has the shortest ultimate charge time?",
+        "answers": ["Tracer", "Zarya", "Genji", "Junkrat"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is the leader of Junkertown and a former mech fighter?",
+        "answers": ["Junker Queen", "Junkrat", "Roadhog", "Reaper"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is a ruthless leader of Talon who believes that conflict is the only way to strengthen humanity?",
+        "answers": ["Genji", "Moira", "Doomfist", "Sombra"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a new model of OR15 defense robot?",
+        "answers": ["Reinhardt", "Echo", "Bastion", "Orisa"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is an advanced robot created by Dr. Mina Liao?",
+        "answers": ["Torbjörn", "Echo", "Orisa", "Lifeweaver"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is a freedom fighter who stole Vishkar’s sonic technology?",
+        "answers": ["Torbjörn", "Symmetra", "Lucio", "Mei"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a shrine maiden and a protector of Kanezaka?",
+        "answers": ["Kiriko", "Genji", "Tracer", "Hanzo"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero was fascinated by the potential of hard-light to manipulate life forms and create new ones?",
+        "answers": ["Symmetra", "Ana", "Moira", "Lifeweaver"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is a bounty hunter who was caught by Overwatch during a sting operation and given a choice to join them or rot in prison?",
+        "answers": ["Hanzo", "Cassidy", "Ashe", "Ramattra"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is a brilliant astrophysicist who was exposed to a black hole experiment that gave him the ability to manipulate gravity?",
+        "answers": ["Winston", "Reaper", "Sigma", "Mercy"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a scientist who was raised on the moon and joined Overwatch?",
+        "answers": ["Winston", "Wrecking Ball", "Mercy", "Moira"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is a climatologist and an adventurer who uses her endothermic blaster and weather drones to study and preserve the environment?",
+        "answers": ["Kiriko", "Ramattra", "Mei", "Lucio"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a Canadian soldier and an Overwatch agent who leads the team in Overwatch 2",
+        "answers": ["Soldier 76", "Reaper", "Mercy", "Sojourn"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is an engineer and an inventor who helped design many weapons for Overwatch?",
+        "answers": ["Ana", "Torbjörn", "Winston", "Echo"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero was the head of medical research for Overwatch?",
+        "answers": ["Moira", "Mercy", "Lifeweaver", "Kiriko"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is the heir of the Shimada clan but who left his family after killing his brother?",
+        "answers": ["Reaper", "Genji", "Sombra", "Hanzo"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is a crusader who fights with honor and courage?",
+        "answers": ["Zarya", "Pharah", "Reinhardt", "Brigitte"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero was student and brother to Zenyatta?",
+        "answers": ["Ramattra", "Echo", "Hanzo", "Tracer"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is a Russian soldier and a weightlifting champion?",
+        "answers": ["Reinhardt", "Zarya", "Baptiste", "Soldier 24"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero escaped from the lunar colony with Winston and built a combat mech from junk parts?",
+        "answers": ["Roadhog", "Sigma", "D.Va", "Wrecking Ball"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero is a combat omnic who developed a curious personality and a fondness for nature?",
+        "answers": ["Echo", "Torbjörn", "Bastion", "Orisa"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero was saved by Overwatch and trained by Zenyatta to find peace with himself?",
+        "answers": ["Ramattra", "Genji", "Reaper", "Widowmaker"],
+        "correct": 1
+    },
+    {
+        "question": "Which hero is a vigilante and a former leader of Overwatch who was presumed dead after an explosion at the headquarters?",
+        "answers": ["Soldier 76", "Ana", "Winston", "Sojourn"],
+        "correct": 0
+    },
+    {
+        "question": "Which hero is an architect and an agent of Vishkar Corporation?",
+        "answers": ["Ana", "Dr. Mina Liao", "Maximillian", "Symmetra"],
+        "correct": 3
+    },
+    {
+        "question": "Which hero was brainwashed and turned into a cold-blooded killer by Talon?",
+        "answers": ["Moira", "Reaper", "Widowmaker", "Sigma"],
+        "correct": 2
+    },
+    {
+        "question": "Which hero is a geneticist and a member of Talon who seeks to unlock the secrets of life?",
+        "answers": ["Mercy", "Moira", "Lifeweaver", "Doomfist"],
+        "correct": 1
+    }
+]
