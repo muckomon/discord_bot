99a95ad5e7c0d54cc2ae23365167360541f17bd0
muckomon <112518552+muckomon@users.noreply.github.com>
2023-05-18 16:05:23 +0200

    rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -59,12 +59,13 @@ class TriviaView(View):
         self.stop()
 
 
+
 # Slash command for to start the trivia game
 @bot.tree.command(name='trivia')
 @app_commands.choices(category=[
     app_commands.Choice(name='facts', value=1),
     app_commands.Choice(name='silhouettes', value=2),
-    app_commands.Choice(name='maps', value=3)
+    #app_commands.Choice(name='maps', value=3)
 ])
 @app_commands.choices(difficulty=[
     app_commands.Choice(name='easy', value=1),
@@ -77,7 +78,7 @@ async def trivia(interaction: discord.Interaction, category: app_commands.Choice
     difficulty = difficulty.name
     rounds = rounds
 
-    if rounds < 1 or rounds > 10:
+    if rounds < 1 or rounds > 36:
         await interaction.response.send_message('Invalid number of rounds. Please choose between 1 and 10!')
         return
     score = 0
@@ -88,55 +89,146 @@ async def trivia(interaction: discord.Interaction, category: app_commands.Choice
         questions_copy = None
         if category == 'facts':
             questions_copy = questions.facts.copy()
+            # Get a random question
+            random_question = random.choice(questions_copy)
+
+            # Get the question
+            question = random_question[difficulty]
+
+            # Get the alternatives
+            answers = random_question['answers']
+
+            # Get correct answer
+            correct_answer = random_question['correct']
+
+            # Get incorrect answers
+            incorrect_answers = answers.copy()
+            incorrect_answers.remove(correct_answer)
+
+            # Shuffle the answers
+            random.shuffle(answers)
+
+            # Get the index of the correct answer
+            correct_index = answers.index(correct_answer)
+
+            # Get the correct letter for the answer
+            correct_letter = ['A', 'B', 'C', 'D'][correct_index]
+
+            # Make the answer text for the question
+            answer_text = '\n'.join([f"{letter}) {answer}" for letter, answer in zip(["A", "B", "C", "D"], answers)])
+
+            # Remove the question from the question data copy to not repeat the same question
+            questions_copy.remove(random_question)
+
+            # Make the embed for the question
+            embed = discord.Embed(title=f'Round {i + 1}', description=question)
+            embed.add_field(name='Answers', value=answer_text)
+            view = TriviaView(question, correct_letter)
+            await interaction.followup.send(embed=embed, view=view)
+            await view.wait()
+
+            # Check if the user answered correctly
+            if view.response == correct_letter:
+                await interaction.followup.send(random.choice(responses.positive))
+                score += 1
+            else:
+                await interaction.followup.send(random.choice(responses.negative))
+
         elif category == 'silhouettes':
             questions_copy = questions.silhouettes.copy()
+            # Get a random question
+            random_question = random.choice(questions_copy)
+
+            # Get the question
+            question = random_question[difficulty]
+
+            # Get the alternatives
+            answers = random_question['answers']
+
+            # Get correct answer
+            correct_answer = random_question['correct']
+
+            # Get incorrect answers
+            incorrect_answers = answers.copy()
+            incorrect_answers.remove(correct_answer)
+
+            # Shuffle the answers
+            random.shuffle(answers)
+
+            # Get the index of the correct answer
+            correct_index = answers.index(correct_answer)
+
+            # Get the correct letter for the answer
+            correct_letter = ['A', 'B', 'C', 'D'][correct_index]
+
+            # Make the answer text for the question
+            answer_text = '\n'.join([f"{letter}) {answer}" for letter, answer in zip(["A", "B", "C", "D"], answers)])
+
+            # Remove the question from the question data copy to not repeat the same question
+            questions_copy.remove(random_question)
+
+            # Make the embed for the question
+            embed = discord.Embed(title=f'Round {i + 1}')
+            embed.add_field(name='Answers', value=answer_text)
+            embed.set_image(url=question)
+            view = TriviaView(question, correct_letter)
+            await interaction.followup.send(embed=embed, view=view)
+            await view.wait()
+
+            # Check if the user answered correctly
+            if view.response == correct_letter:
+                await interaction.followup.send(random.choice(responses.positive))
+                score += 1
+            else:
+                await interaction.followup.send(random.choice(responses.negative))
+
         elif category == 'maps':
             questions_copy = questions.maps.copy()
+            # Get a random question
+            random_question = random.choice(questions_copy)
 
-        # Get a random question
-        random_question = random.choice(questions_copy)
+            # Get the question
+            question = random_question[difficulty]
 
-        # Get the question
-        question = random_question[difficulty]
+            # Get the alternatives
+            answers = random_question['answers']
 
-        # Get the alternatives
-        answers = random_question['answers']
+            # Get correct answer
+            correct_answer = random_question['correct']
 
-        # Get correct answer
-        correct_answer = random_question['correct']
+            # Get incorrect answers
+            incorrect_answers = answers.copy()
+            incorrect_answers.remove(correct_answer)
 
-        # Get incorrect answers
-        incorrect_answers = answers.copy()
-        incorrect_answers.remove(correct_answer)
+            # Shuffle the answers
+            random.shuffle(answers)
 
-        # Shuffle the answers
-        random.shuffle(answers)
+            # Get the index of the correct answer
+            correct_index = answers.index(correct_answer)
 
-        # Get the index of the correct answer
-        correct_index = answers.index(correct_answer)
+            # Get the correct letter for the answer
+            correct_letter = ['A', 'B', 'C', 'D'][correct_index]
 
-        # Get the correct letter for the answer
-        correct_letter = ['A', 'B', 'C', 'D'][correct_index]
+            # Make the answer text for the question
+            answer_text = '\n'.join([f"{letter}) {answer}" for letter, answer in zip(["A", "B", "C", "D"], answers)])
 
-        # Make the answer text for the question
-        answer_text = '\n'.join([f"{letter}) {answer}" for letter, answer in zip(["A", "B", "C", "D"], answers)])
+            # Remove the question from the question data copy to not repeat the same question
+            questions_copy.remove(random_question)
 
-        # Remove the question from the question data copy to not repeat the same question
-        questions_copy.remove(random_question)
+            # Make the embed for the question
+            embed = discord.Embed(title=f'Round {i + 1}')
+            embed.add_field(name='Answers', value=answer_text)
+            embed.set_image(url=question)
+            view = TriviaView(question, correct_letter)
+            await interaction.followup.send(embed=embed, view=view)
+            await view.wait()
 
-        # Make the embed for the question
-        embed = discord.Embed(title=f'Round {i + 1}', description=question)
-        embed.add_field(name='Answers', value=answer_text)
-        view = TriviaView(question, correct_letter)
-        await interaction.followup.send(embed=embed, view=view)
-        await view.wait()
-
-        # Check if the user answered correctly
-        if view.response == correct_letter:
-            await interaction.followup.send('Correct!')
-            score += 1
-        else:
-            await interaction.followup.send(f'Wrong! The correct answer was {correct_letter}) {correct_answer}.')
+            # Check if the user answered correctly
+            if view.response == correct_letter:
+                await interaction.followup.send(random.choice(responses.positive))
+                score += 1
+            else:
+                await interaction.followup.send(random.choice(responses.negative))
 
     # Send goodbye message
     await interaction.followup.send(f'Game over! Your final score is {score}/{rounds}')
